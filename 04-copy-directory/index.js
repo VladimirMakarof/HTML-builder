c7a47a239b0a9654e5141a6104252cd94bbdcaba
Vladimir_Makarov <stan13.09@mail.ru>
2022-05-23 21:05:57 +0300

    feat: done task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { mkdir, readdir, copyFile, unlink } = require('fs/promises');
+const path = require('path');
 
+(async function() {
+  try {
+    await mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+    const files = await readdir(path.join(__dirname, 'files'));
+    const filesCopy = await readdir(path.join(__dirname, 'files-copy'));
+
+    if (filesCopy.length) {
+      for (const fileCopy of filesCopy) {
+        unlink(path.join(__dirname, 'files-copy', fileCopy));
+      }
+    }
+
+    for (const file of files) {
+      copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
